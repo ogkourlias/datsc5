0145c43aaea4f103705974aa45f341b839b162ff
ogkourlias <orfeasgkourlias@gmail.com>
2025-09-15 15:36:11 +0200

    Added two placeholder classes for regression and linear regression

---
+++ regression/regr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+    usage:
+        python3 ./regr.py
+"""
+
+# METADATA VARIABLES
+__author__ = "Orfeas Gkourlias"
+__status__ = "WIP"
+__version__ = "0.1"
+
+# IMPORTS
+import sys
+
+
+# CLASSES
+class Regr():
+    """ Regression class """
+
+# MAIN
+def main(args):
+    """ Main function """
+    # FINISH
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
