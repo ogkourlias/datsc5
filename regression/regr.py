579674fcf5fac586d490449c96d7b7c0cefa93c5
ogkourlias <orfeasgkourlias@gmail.com>
2025-09-29 15:16:59 +0200

    Merged and renamed functions where linear and logistic overlap. Added fit function which was previously called gradient descent.

--- regression/regr.py
+++ regression/regr.py
@@ -39,12 +39,80 @@ class Regr:
         theta = np.zeros(x.shape[1])
         return x, y, theta
 
-    def get_cost(self, x, y, theta):
+    def compute_loss(self, y_hat, y):
         """
-        Return mean-squared error (MSE) of model predictions vs. actual Ys
+        Return array of residuals between Y predictions and actual Y's
         """
-        mse_vec = sum((x @ theta - y) ** 2) / (2 * len(y))
-        return mse_vec
+        loss = y_hat - y
+        return loss
+    
+    def compute_cost(self, y_hat, y, method, theta, lambda_=0.5):
+        """
+        Return MSE cost for linear regression with ridge regularization.
+        Or return log cost for logistic regression with ridge regularization.
+        """
+        m = len(y)
+        reg = (lambda_ / (2 * m)) * np.sum(theta[1:] ** 2)
+        if method == "linear":
+            residuals = y_hat - y
+            mse = np.sum(residuals ** 2) / (2 * m)
+            cost = mse + reg
+            return cost
+        elif method == "logistic":
+            log = -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+            cost = log + reg
+            return cost
+    
+    def predict(self, x, theta, method = "linear"):
+        """
+        Predict Ys using parameters (x matrix) and theta (coefficients)
+        """
+        y_hat = x @ theta
+        if method == "logistic":
+            # IMPORTANT: These are probabilities, NOT the class labels itself.
+            y_hat = self.sigmoid(y_hat)
+        return y_hat
+    
+    def odds_to_class(self, y_hat, threshold = 0.5):
+        """
+        Converts logistic regression odds to actual binary classifiers
+        """
+        return np.array([1.0 if h >= threshold else 0.0 for h in y_hat])
+    
+    def compute_gradient(self, x, y, y_hat, theta, lambda_=0.5):
+        """
+        Compute gradient for linear or logistic regression with optional ridge regularization.
+        """
+        m = len(y)
+        loss = y_hat - y  # shape (m, )
+
+        # Base gradient
+        grad = (x.T @ loss) / m
+
+        # Ridge regularization (exclude bias)
+        grad[1:] += (lambda_ / m) * theta[1:]
+        return grad
+    
+    
+    def fit(self, x, y, theta, alpha=0.01, num_iters=500, method = "linear", regular="ridge", lambda_ = 0.5):
+        """
+        Fitting model with or without regularization.
+        L1 = Lasso (not implemented).
+        L2 = Ridge (default).
+
+        Important: Be conscious over whether the input x and theta should be scaled.
+        """
+        cost_history = np.zeros(num_iters)
+
+        if regular == "ridge":
+            for i in range(num_iters):
+                y_hat = self.predict(x, theta, method)
+                grad = self.compute_gradient(x, y, y_hat, theta, lambda_)
+                theta = theta - alpha * grad
+                new_y_hat = self.predict(x, theta, method)
+                cost_history[i] = self.compute_cost(new_y_hat, y, method, theta, lambda_)
+        return theta, cost_history
+    
 
     ### Scaling ###
     def scale_x(self, x):
@@ -76,105 +144,14 @@ class Regr:
         theta_original[1:] = theta[1:] / scaler.scale_
 
         return theta_original
-
-    ### Linear Regression Gradient Descent
-    def gradient_descent_linear(
-        self, x, y, theta, alpha=0.001, num_iters=100, regular="ridge", lambda_=0.5
-    ):
-        """
-        Gradient descent with or without regularization.
-        L1 = Lasso.
-        L2 = Ridge.
-        Otherwise, no regularization.
-        """
-        # initialize list of costs
-        m = len(y)
-        cost_history = np.zeros(num_iters)
-        if regular == "ridge":
-            for i in range(num_iters):
-                grad = (x.T @ ((x @ theta) - y)) / m
-                reg = (lambda_ / m) * theta
-                reg[0] = 0
-                theta = theta - alpha * (grad + reg)
-                cost_history[i] = (
-                    np.sum((x @ theta - y) ** 2) + lambda_ * np.sum(theta[1:] ** 2)
-                ) / (2 * m)
-        else:
-            for i in range(0, num_iters):
-                theta = theta - alpha / y.size * (x.T @ ((x @ theta) - y))
-                cost_history[i] = sum((x @ theta - y) ** 2) / (2 * len(y))
-
-        return theta, cost_history
-
+    
+    # Logistic Specific
     def sigmoid(self, z):
         """
         Return sigmoid of Z
         """
         sig = 1 / (1 + np.exp(-z))
         return sig
-
-    def compute_cost_logistic(self, x, y, theta, lambda_):
-        """
-        Compute cost for logistic regression with regularization.
-
-        Parameters:
-        X:  Input feature matrix (m x n)
-        y: True labels vector (m,)
-        theta: Parameters vector (n,)
-        lambda_: Regularization parameter
-
-        Return:
-        J:Cost value
-        """
-        m = len(y)
-        h = self.sigmoid(x.dot(theta))
-        reg = (lambda_ / (2 * m)) * np.sum(theta[1:] ** 2)
-        cost = -1 / m * (y.dot(np.log(h)) + (1 - y).dot(np.log(1 - h)))
-        return cost + reg
-
-    def gradient_descent_logistic(self, x, y, theta, alpha, num_iters, lambda_):
-        """
-        Perform gradient descent to find optimal theta.
-
-        Parameters:
-        X: Input feature matrix (m x n)
-        y: True labels vector (m,)
-        theta: Initial parameters vector (n,)
-        alpha: Learning rate
-        num_iters: Number of iterations
-        lambda_:  Regularization parameter
-
-        Return:
-        theta:  Updated parameters vector
-        J_history:  History of cost values
-        """
-
-        m = len(y)
-        cost_history = np.zeros(num_iters)
-        for i in range(num_iters):
-            grad = (x.T @ ((self.sigmoid(x @ theta)) - y)) / m
-            reg = (lambda_ / m) * theta
-            reg[0] = 0
-            theta = theta - alpha * (grad + reg)
-            cost_history[i] = self.compute_cost_logistic(x, y, theta, lambda_)
-        return theta, cost_history
-
-    def predict_logistic(self, x, theta, threshold=0.5):
-        """
-        Predict whether the label is 0 or 1 using learned logistic regression parameters theta.
-
-        Parameters:
-        X: Input feature matrix (m x n)
-        theta: Parameters vector (n,) (computed by compute cost and gradient descentl)
-        threshold: Threshold for prediction
-
-        Return:
-        p: Predicted labels vector (m,)
-        """
-        pred = self.sigmoid(x.dot(theta))
-        p = [1.0 if h >= threshold else 0.0 for h in pred]
-        return p
-
 
 # MAIN
 def main(args):
