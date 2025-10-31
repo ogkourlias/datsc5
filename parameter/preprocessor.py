1013f0f6c9e71943586ca65b70b06e1a9e7e996a
ogkourlias <orfeasgkourlias@gmail.com>
2025-10-31 15:56:32 +0100

    Added start of preprocessor class

---
+++ parameter/preprocessor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+    usage:
+        python3 orfeas_gkourlias boink.py
+"""
+
+# METADATA VARIABLES
+__author__ = "Orfeas Gkourlias"
+__status__ = "WIP"
+__version__ = "0.1"
+
+# IMPORTS
+import sys
+
+# CLASSES
+class Preprocessor():
+    """ Desc """
+    def __init__(self) -> None:
+        pass
+
+# MAIN
+def main(args):
+    """ Main function """
+    # FINISH
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
